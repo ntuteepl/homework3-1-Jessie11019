e375ae5a016f7ca9ebe02ad2cd5974fc53caf11b
Jessie11019 <146058017+Jessie11019@users.noreply.github.com>
2023-10-22 10:23:19 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,45 @@
+#include "stdio.h"
+
+int time[25];
+
+
+int main()
+{
+int s1,d1,s2,d2,s3,d3;
+scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+int schedule[3][2] = {{s1,d1},{s2,d2},{s3,d3}};
+
+int car_number = 0;
+    int i = 0;
+    int j = 0;
+
+    for(i=0; i< 3; i++)
+    {
+        int start_time = schedule[i][0];
+        int end_time = schedule[i][1];
+
+        for(j=start_time; j<end_time;j++)
+        {
+            time[j] +=1;
+        }
+    }
+
+    int k = 0;
+
+    for(k = 0; k <= 24; k++)
+    {
+        if(time[k] > car_number)
+        {
+            car_number = time[k];
+        }
+    }
+
+    printf("the car number is %d\n", car_number);
+
+
+
+
+
+    return 0;
+}
 
