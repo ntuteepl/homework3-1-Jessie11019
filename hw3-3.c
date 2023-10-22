d0cd2d72b8875cb318298113aba1c53521681a44
Jessie11019 <146058017+Jessie11019@users.noreply.github.com>
2023-10-22 22:50:32 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+int main()
+{
+    int i=0,n=0;
+    scanf("%d %d",&i,&n);
+
+    int a=(i/1000)+((i/100)%10)+((i/10)%10)+(i%10);
+    int b=(n/1000)+((n/100)%10)+((n/10)%10)+(n%10);
+
+    if(a>b)
+    {
+        printf("%d",n);
+    }
+    if(a<b)
+    {
+        printf("%d",i);
+    }
+    else if(a==b)
+    {
+        if(i>n)
+        {
+            printf("%d",n);
+        }
+        if(i<n)
+        {
+            printf("%d",i);
+        }
+    }
+return 0;
+}
